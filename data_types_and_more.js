528022c050da75855f39c843de156e69959e3ba8
Mantas Lingaitis <mantasling@gmail.com>
2022-11-14 21:08:54 +0200

    > Data type Js finished

--- data_types_and_more.js
+++ data_types_and_more.js
@@ -1,13 +1,34 @@
 /* 
 >> Documentation for JavaScript
     [] data types
+    [] comparison operators
+    [] variables
 >> Page: 23
 >> Link to doc: https://docs.google.com/document/d/1NoFPu4Z9_kNf3hDG59YhuLVXH1JKc71kgO7CtFawtDw/edit?skip_itp2_check=true#
 */
-var data_number = 10
-var data_string = "Text"
-var data_bool = true
 
-console.log(data_number)
-console.log(data_string)
-console.log(data_bool)
+// Example of some data types
+console.log(">>> Some data types: ")
+console.log("A number: ", 10)
+console.log("A piece of text")
+console.log("Boolean: ", true)
+
+// Comparison operator examples 
+console.log(">>> Comparison operators: ")
+
+console.log("Is 10 >= 5; ", 10 >= 5)
+console.log("Is 55 equal value and type to 55; ", 55 === 55)
+console.log("Is \"55\" equal value and type to 55; ", "55" === 55)
+console.log("Is \"55\" equal value and NOT type to 55; ", "55" == 55)
+
+// Example of variable declaration and use
+var my_var_number = 10
+var my_var_string = "Text"
+var my_var_bool = true
+var my_var_combo = my_var_number + my_var_string + my_var_bool;
+
+console.log(">>> Variables: ")
+console.log(my_var_number)
+console.log(my_var_string)
+console.log(my_var_bool)
+console.log(my_var_combo)
