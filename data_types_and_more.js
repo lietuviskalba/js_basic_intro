d6d13b9ed20cd9ddad19333bfeaa6861e1985a1e
Mantas Lingaitis <mantasling@gmail.com>
2022-11-14 20:48:51 +0200

    >>> CSS update >> Index update >> Js data types added

---
+++ data_types_and_more.js
@@ -0,0 +1,13 @@
+/* 
+>> Documentation for JavaScript
+    [] data types
+>> Page: 23
+>> Link to doc: https://docs.google.com/document/d/1NoFPu4Z9_kNf3hDG59YhuLVXH1JKc71kgO7CtFawtDw/edit?skip_itp2_check=true#
+*/
+var data_number = 10
+var data_string = "Text"
+var data_bool = true
+
+console.log(data_number)
+console.log(data_string)
+console.log(data_bool)
