95d546d2db6451ba5ea95ae05e426ebb314dc5de
Mantas Lingaitis <mantasling@gmail.com>
2022-11-15 21:37:00 +0200

    > Arrays added

---
+++ scripts/arrays.js
@@ -0,0 +1,17 @@
+/* 
+>> Documentation for JavaScript
+    [] arrays
+>> Page: 29
+>> Link to doc: https://docs.google.com/document/d/1NoFPu4Z9_kNf3hDG59YhuLVXH1JKc71kgO7CtFawtDw/edit?skip_itp2_check=true#
+*/
+
+var my_array_number = [5,10,15,100,-5]
+var my_array_string = ["text one", "text two", "text three"]
+var my_array_bool = [true,true,false,true]
+var matrix_array = [[11,22,33],[44,55,66],[77,88,99]]
+
+console.log("Array example: ")
+console.log("Array first position: ", my_array_number[0])
+console.log("Array second position: ", my_array_string[1])
+console.log("Array third position: ", my_array_bool[2])
+console.log("Array matrix position 1 and 11: ", matrix_array[2][1])
