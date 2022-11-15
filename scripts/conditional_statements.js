a9f46f8ea6126bde4938a7b1f17472ef4432a909
Mantas Lingaitis <mantasling@gmail.com>
2022-11-15 20:54:56 +0200

    > Documentation update

--- scripts/conditional_statements.js
+++ scripts/conditional_statements.js
@@ -1,6 +1,7 @@
 /* 
 >> Documentation for JavaScript
     [] conditional statements (if else)
+    [] logical operators
 >> Page: 24
 >> Link to doc: https://docs.google.com/document/d/1NoFPu4Z9_kNf3hDG59YhuLVXH1JKc71kgO7CtFawtDw/edit?skip_itp2_check=true#
 */
