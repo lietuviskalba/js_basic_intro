dbcfba8cbff1d01bc3ee2927c23a8e1920fb7c93
Mantas Lingaitis <mantasling@gmail.com>
2022-11-15 21:19:29 +0200

    > Functions added

---
+++ scripts/functions.js
@@ -0,0 +1,20 @@
+/* 
+>> Documentation for JavaScript
+    [] functions
+>> Page: 26
+>> Link to doc: https://docs.google.com/document/d/1NoFPu4Z9_kNf3hDG59YhuLVXH1JKc71kgO7CtFawtDw/edit?skip_itp2_check=true#
+*/
+
+function addTwoNumbers(num1, num2) {
+    return num1 + num2;
+}
+
+var my_text_add_question_mark = function (text) {
+    return text + "?";
+}
+
+
+console.log("Function example: ")
+console.log("Function add two numbers: ", addTwoNumbers(5, 10))
+console.log(my_text_question_mark("Some text is here"))
+
